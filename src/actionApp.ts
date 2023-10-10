b71753128fc1a02e1db9660597237dcab5323663
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2023-10-10 11:36:24 -0700

    Multiple task types

--- src/actionApp.ts
+++ src/actionApp.ts
@@ -4,47 +4,67 @@ import {
   TurnContext,
   MessagingExtensionAction,
   MessagingExtensionActionResponse,
+  TaskModuleRequest,
+  TaskModuleResponse,
 } from "botbuilder";
 
 export class ActionApp extends TeamsActivityHandler {
   //Action
-  public async handleTeamsMessagingExtensionSubmitAction(
+  public override async handleTeamsMessagingExtensionSubmitAction(
     context: TurnContext,
     action: MessagingExtensionAction
   ): Promise<MessagingExtensionActionResponse> {
     // The user has chosen to create a card by choosing the 'Create Card' context menu command.
-    const data = action.data;
-    const attachment = CardFactory.adaptiveCard({
-      $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
-      type: "AdaptiveCard",
-      version: "1.4",
-      body: [
+    console.log(`HANDLE SUBMIT ACTION, action: ${JSON.stringify(action)}`);
+
+    const commandId: string = action.commandId;
+    const commandData = action.data;
+    
+    switch (commandId) {
+      case "createCard":
         {
-          type: "TextBlock",
-          text: `${data.title}`,
-          wrap: true,
-          size: "Large",
-        },
-        {
-          type: "TextBlock",
-          text: `${data.subTitle}`,
-          wrap: true,
-          size: "Medium",
-        },
-        {
-          type: "TextBlock",
-          text: `${data.text}`,
-          wrap: true,
-          size: "Small",
-        },
-      ],
-    });
-    return {
-      composeExtension: {
-        type: "result",
-        attachmentLayout: "list",
-        attachments: [attachment],
-      },
-    };
+          const attachment = CardFactory.adaptiveCard({
+            $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
+            type: "AdaptiveCard",
+            version: "1.4",
+            body: [
+              {
+                type: "TextBlock",
+                text: `${commandData.title}`,
+                wrap: true,
+                size: "Large",
+              },
+              {
+                type: "TextBlock",
+                text: `${commandData.subTitle}`,
+                wrap: true,
+                size: "Medium",
+              },
+              {
+                type: "TextBlock",
+                text: `${commandData.text}`,
+                wrap: true,
+                size: "Small",
+              },
+            ],
+          });
+          return {
+            composeExtension: {
+              type: "result",
+              attachmentLayout: "list",
+              attachments: [attachment],
+            },
+          };
+        }
+      
+      default:
+        return;
+    }  
+  }
+
+  public override handleTeamsTaskModuleFetch(context: TurnContext, taskModuleRequest: TaskModuleRequest): Promise<TaskModuleResponse> {
+    console.log(`TASK MODULE FETCH. Task module request: ${JSON.stringify(taskModuleRequest)}`);
+
+    return;
   }
 }
