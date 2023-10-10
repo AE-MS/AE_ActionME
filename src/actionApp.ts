40a500e313bf88f046ee8a6857fc8c7a942e3c6d
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2023-10-10 16:32:52 -0700

    Cards and dialogs and such

--- src/actionApp.ts
+++ src/actionApp.ts
@@ -8,6 +8,52 @@ import {
   TaskModuleResponse,
 } from "botbuilder";
 
+const urlDialogTriggerValue = "requestUrl";
+const cardDialogTriggerValue = "requestCard";
+const messagePageTriggerValue = "requestMessage";
+const noResponseTriggerValue = "requestNoResponse";
+
+const adaptiveCardBotJson = {
+  "contentType": "application/vnd.microsoft.card.adaptive",
+  "content": {
+      "type": "AdaptiveCard",
+      "body": [
+          {
+              "type": "TextBlock",
+              "text": "Here is a ninja cat:"
+          },
+          {
+              "type": "Image",
+              "url": "http://adaptivecards.io/content/cats/1.png",
+              "size": "Medium"
+          }
+      ],
+      "actions": [
+          {
+              "data": { data: urlDialogTriggerValue },
+              "type": "Action.Submit",
+              "title": "Request URL Dialog"
+          },
+          {
+            "data": { data: cardDialogTriggerValue },
+            "type": "Action.Submit",
+            "title": "Request Card Dialog"
+          },
+          {
+            "data": { data: messagePageTriggerValue },
+            "type": "Action.Submit",
+            "title": "Request Message"
+          },
+          {
+            "data": { data: noResponseTriggerValue },
+            "type": "Action.Submit",
+            "title": "Request No Response (close Dialog)"
+          },
+      ],
+      "version": "1.0"
+  }
+}
+
 export class ActionApp extends TeamsActivityHandler {
   //Action
   public override async handleTeamsMessagingExtensionSubmitAction(
@@ -19,6 +65,12 @@ export class ActionApp extends TeamsActivityHandler {
 
     const commandId: string = action.commandId;
     const commandData = action.data;
+
+    if (commandData.data === urlDialogTriggerValue) {
+      return this.createUrlTaskModuleMEResponse();
+    } else if (commandData.data === cardDialogTriggerValue) {
+      return this.createCardTaskModuleMEResponse();
+    }
     
     switch (commandId) {
       case "createCard":
@@ -47,6 +99,16 @@ export class ActionApp extends TeamsActivityHandler {
                 size: "Small",
               },
             ],
+            actions: [
+              {
+                type: "Action.Submit",
+                title: "Show URL Task Module",
+                value: {
+                  type: 'task/fetch',
+                  data: urlDialogTriggerValue
+                }
+              },
+            ],
           });
           return {
             composeExtension: {
@@ -62,8 +124,66 @@ export class ActionApp extends TeamsActivityHandler {
     }  
   }
 
+  private getRandomIntegerBetween(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  private createUrlTaskModuleMEResponse(): Promise<MessagingExtensionActionResponse> {
+    return Promise.resolve({
+      task: {
+        type: 'continue',
+        value: {
+          url: `https://helloworld36cffe.z5.web.core.windows.net/index.html?randomNumber=${this.getRandomIntegerBetween(1, 1000)}#/tab`,
+          fallbackUrl: "https://thisisignored.example.com/",
+          height: 510,
+          width: 450,
+          title: "URL Dialog",
+        }
+      }
+    });
+  }
+
+  private createCardTaskModuleMEResponse(): Promise<MessagingExtensionActionResponse> {
+    return Promise.resolve({
+      task: {
+        type: 'continue',
+        value: {
+          card: adaptiveCardBotJson,
+          height: 510,
+          width: 450,
+          title: "Adaptive Card Dialog",
+        }
+      }
+    });
+  }
+
+  // Called when the user selects a commmand from the ME's command list when activating an ME
+  protected override handleTeamsMessagingExtensionFetchTask(_context: TurnContext, action: MessagingExtensionAction): Promise<MessagingExtensionActionResponse> {
+    console.log(`ME FETCH TASK. Action: ${JSON.stringify(action)}`);
+
+    switch (action.commandId) {
+      case "fetchCardDialog" : {
+        return this.createCardTaskModuleMEResponse();
+      }
+      case "fetchUrlDialog" : {
+        return this.createUrlTaskModuleMEResponse();
+      }
+      default : {
+        console.log(`Unknown commandId: ${action.commandId}`);
+      }
+    }
+
+    return;
+  }
+
   public override handleTeamsTaskModuleFetch(context: TurnContext, taskModuleRequest: TaskModuleRequest): Promise<TaskModuleResponse> {
     console.log(`TASK MODULE FETCH. Task module request: ${JSON.stringify(taskModuleRequest)}`);
+
+    return;
+  }
+
+  public override handleTeamsTaskModuleSubmit(_context: TurnContext, taskModuleRequest: TaskModuleRequest): Promise<TaskModuleResponse> {
+    console.log(`HANDLING DIALOG SUBMIT. Task module request: ${JSON.stringify(taskModuleRequest)}`);
 
     return;
   }
--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,13 @@ const actionApp = new ActionApp();
 
 // Create HTTP server.
 const server = restify.createServer();
+
+server.on('after', function(req, res, route, error) {
+  console.log("------------------------")
+  console.log(req.route.path)
+  console.log(req.body)
+});
+
 server.use(restify.plugins.bodyParser());
 server.listen(process.env.port || process.env.PORT || 3978, () => {
   console.log(`\nBot Started, ${server.name} listening to ${server.url}`);
